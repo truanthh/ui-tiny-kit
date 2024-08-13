760a88a3070395ca2d543e670eb361cba1ac8c61
truanthh <aquapyr@gmail.com>
2024-08-13 09:02:50 +0300

    displaying code component/ copy button done
    sad

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,57 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
+import { RouterLink, RouterView, useRoute } from "vue-router";
+import { ref, watch } from "vue";
+
+import { Icon } from "@Iconify/vue";
 import LayoutSidebar from "@/components/Layout/Sidebar.vue";
+import Button from "@/components/Button.vue";
+
+// components raw text data
+import CheckboxString from "@/components/Checkbox/Checkbox.vue?raw";
+import ButtonString from "@/components/Button.vue?raw";
+import TypographyString from "@/views/TypographyView.vue?raw";
+
+const route = useRoute();
+const currentPath = ref(route.path);
+const compString = ref(null);
+
+const componentStrings = {
+  "/button": ButtonString,
+  "/checkbox": CheckboxString,
+  "/typography": TypographyString,
+};
+
+watch(route, (newRoute) => {
+  currentPath.value = newRoute.path;
+  compString.value = componentStrings[currentPath.value];
+});
+
+const copyToClipboard = async () => {
+  try {
+    await navigator.clipboard.writeText(codeText.value.innerText);
+  } catch (error) {
+    console.error("Не удалось скопировать текст!");
+  }
+};
 </script>
 
 <template>
   <div class="container">
     <LayoutSidebar />
     <div class="content">
+      <Button
+        class="copy"
+        label="primary"
+        color="primary"
+        :rounded="true"
+        :outlined="true"
+        icon="ph:copy"
+        @click="copyToClipboard"
+      />
       <RouterView />
+    </div>
+    <div class="code" ref="codeText">
+      <pre> <code> {{ compString}} </code> </pre>
     </div>
   </div>
 </template>
@@ -20,14 +64,35 @@ import LayoutSidebar from "@/components/Layout/Sidebar.vue";
   -webkit-font-smoothing: antialised;
   -moz-osx-font-smoothing: grayscale;
 }
-
+.copy {
+  position: absolute;
+  top: 10px;
+  right: 60px;
+  // margin-right: 75px;
+  margin-top: 45px;
+}
+.container {
+  display: grid;
+  grid-template-columns: 1.3fr 1fr;
+  height: 100vh;
+}
 .content {
+  position: relative;
   max-width: 1400px;
   margin-left: 250px;
   padding: 30px;
   transition: 0.2s;
+  overflow-y: auto;
   &_full {
     margin-left: 0;
   }
+}
+.code {
+  height: 100%;
+  width: 100%;
+  padding: 10px;
+  overflow-y: auto;
+  background-color: #f5f5f5;
+  border-left: solid 2px black;
 }
 </style>
