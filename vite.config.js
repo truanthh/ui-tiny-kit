83d8a9a2aa4a18be4846e9749ec33cc7eab7b47f
truanthh <aquapyr@gmail.com>
2024-08-20 18:25:57 +0300

    yes

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-  base: "/",
+  base: "/ui-tiny-kit/",
   plugins: [vue()],
   resolve: {
     alias: {
